b517c213ebd819ee1dbdc7318b945b0272b70792
fknr1337 <underjedi5@yandex.ru>
2021-06-08 22:38:37 +0300

    new files

---
+++ CREATE.sql
@@ -0,0 +1,38 @@
+create table if not exists singers 
+	(id serial primary key, 
+	 name varchar(40) not null unique);
+
+create table if not exists albums 
+	(id serial primary key,  
+	name varchar(40) not null, 
+	year integer not null);
+	
+create table if not exists tracks
+	(id serial primary key,
+	name varchar(40) not null,
+	duration numeric(5,2) check(duration > 0),
+	album_id integer references albums(id));
+
+create table if not exists genre
+	(id serial primary key,
+	name varchar(40) not null unique);
+
+create table genreSinger
+	(singer_id integer references singers(id),
+	genre_id integer references genre(id),
+	 constraint pk primary key (singer_id, genre_id));
+
+create table if not exists collection
+	(id serial primary key,
+	 name varchar(40) not null,
+	 year integer not null);
+
+create table collectionTracks
+	(track_id integer references tracks(id),
+	 collection_id integer references collection(id),
+	 constraint pks primary key (track_id, collection_id));
+
+create table AlbumSingers
+	(singer_id integer references singers(id),
+	 album_id integer references albums(id),
+	 constraint pka primary key (singer_id, album_id))
